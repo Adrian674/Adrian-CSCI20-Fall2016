cdd620ca1c3d2ec7be78dd7f659cd1b3be4bce12
Adrian Gomez <agomezmora001@student.butte.edu>
2016-08-29 15:45:47 +0000

    Lab2 submission

---
+++ lab1/lab1.cpp
@@ -0,0 +1,23 @@
+//Creator: Adrian
+//Date: 8/24/16
+
+#include <iostream>
+using namespace std;
+
+int main()
+{  
+
+cout<<"    ___"<<endl;
+cout<<"     |"<<endl;
+cout<<"     |"<<endl;
+cout<<"     ---(*_*)"<<endl;
+cout<<"          |-----"<<endl;
+cout<<"          |    |"<<endl;
+cout<<"          |    |--"<<endl;
+cout<<"        --=--"<<endl;
+cout<<"        |   |"<<endl;
+cout<<"        |   |"<<endl;
+cout<<"        |   |"<<endl;
+cout<<"        |_  |_"<<endl;
+
+}
---
+++ lab2/lab2.cpp
@@ -0,0 +1,8 @@
+//Creator: Adrian
+//Date: 8/24/16
+
+//computer randomly generates a number between 1-20
+//user guesses the number
+//if the user is right they win 10 pts, if not they lose 1 pt and continue
+//program tells user to guess higher or lower
+//repeat until user guesses number correctly
