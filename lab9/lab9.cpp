1f14f1994c95fa5970424d64c33eb8fa46188ae6
Adrian Gomez <agomezmora001@student.butte.edu>
2016-10-07 21:09:59 +0000

    Lab9 submission

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -7,11 +7,67 @@
  #include <iostream>
  using namespace std;
  
- TemperatureConverter (double kelvin_){
-     
- }
+ class TemperatureConverter {
+  private:
+  double kelvin_;
+  
+  public:
+  TemperatureConverter(){
+   kelvin_=0;
+  }
+  TemperatureConverter(double whatever){
+   kelvin_= whatever;
+  }
+  
+  void SetTempFromKelvin(double kelvin){
+     kelvin_ = kelvin;
+  }
+  double GetTempFromKelvin(){
+   return kelvin_;
+  }
+  void SetTempFromCelsius(double celsius){
+     kelvin_= celsius + 273.15;
+  }
+  void SetTempFromFahrenheit(double fahrenheit){
+     kelvin_= (5 * (fahrenheit -32)/9) + 273.15;
+  }
+  double GetTempAsCelsius(){
+     return kelvin_ - 273.15;
+  }
+  double GetTempAsFahrenheit(){
+      return ((((kelvin_ -273.15)*9)/5) + 32);
+  }
+  void PrintTemperatures(){
+   cout<<"Temperature as Celsius is: ";
+   cout<<GetTempAsCelsius()  << endl;
+   cout<<"Temperature as Fahrenheit is: ";
+   cout<< GetTempAsFahrenheit() << endl
+       <<"Temperature as Kelvin is: ";
+   cout<< GetTempFromKelvin()<<endl;
+  }
+  
+ };
  
  int main(){
      
+    TemperatureConverter temp1; //testing default constructor
+    TemperatureConverter temp2(274); //testing overloaded constructor
+    
+    temp1.PrintTemperatures();
+    temp2.PrintTemperatures();
+    
+    temp1.SetTempFromKelvin(400.15); //testing mutator function
+    cout<<temp1.GetTempFromKelvin()<<endl;//testing accessor function
+    temp1.PrintTemperatures();
+    
+    temp2.SetTempFromCelsius(32); //testing other functions
+    cout<<temp2.GetTempAsCelsius()<<endl;
+    temp2.PrintTemperatures();
+    
+    temp2.SetTempFromFahrenheit(32);
+    cout<<temp2.GetTempAsFahrenheit()<<endl;
+    temp2.PrintTemperatures();
+   
+   return 0;
  }
  
