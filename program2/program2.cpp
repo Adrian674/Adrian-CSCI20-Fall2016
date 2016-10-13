af074f2b5407c875d8860e776868e33c7e41b26a
Adrian Gomez <agomezmora001@student.butte.edu>
2016-10-13 07:24:42 +0000

    Lab13 submission

--- program2/program2.cpp
+++ program2/program2.cpp
@@ -6,32 +6,54 @@
  #include <iostream>
  using namespace std;
  
- double MilesToYards(double Yards){
-  double Miles=0;
-  const double MilesConv=1760;
-  Miles = MilesConv * Yards;
-  
-  return Miles;
-}
-
- double YardsToMiles(double Miles){
-  double Yards=0;
-  const double YardsConv=.0005681818;
-  Yards = YardsConv * Miles;
-  
-  return Yards;
+class DistanceConverter{
+ private:
+ double miles_;
+ 
+ public:
+ DistanceConverter(){
+  miles_=0;
+ }
+ DistanceConverter(double x){
+  miles_=x;
  }
  
- double MilesToMeters (double Meters){
-  double Miles= 0;
-  
-  Miles= 1609.344 * Meters;
-  
-  return Miles;
+ void SetDistFromMiles(double miles){
+  miles_= miles;
  }
+ double GetDistFromMiles(){
+  return miles_;
+ }
+ 
+ void SetDistFromYards(double yards){
+  miles_= yards * .000568182;
+ }
+ double GetDistAsYards(){
+  return miles_*1760;
+ }
+ 
+ void SetDistFromFeet(double feet){
+  miles_= feet * .0001893939;
+ }
+double GetDistAsFeet(){
+ return miles_ * 5280;
+} 
+ 
+ void SetDistFromInches(double inches){
+  miles_= inches * .000015782828;
+ }
+ double GetDistAsInches(){
+  return miles_ * 63360;
+ }
+ 
+ void SetDistFromMeters(double meters){
+  miles_= meters/1609.344;
+ }
+ double GetDistAsMeters(){
+  return miles_ * 1609.344;
+ }
+  
+ };
 int main() {
     
-    cout<<MilesToYards(1)<<endl;
-    cout<<YardsToMiles(1)<<endl;
-    cout<<MilesToMeters(1)<<endl;
 }
